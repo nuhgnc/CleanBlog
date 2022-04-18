95845bb03477f9a21edb8448450002a29c3554bb
nuhgnc <Nuh16358@gmail.com>
2022-04-18 12:17:57 +0300

    public oluştu, view yapısı kuruldu

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
 const app = express();
 
-const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+//VİEW ENGİNE SETUP
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.send(blog);
+//MİDDLEWARE
+app.use(express.static(path.resolve(__dirname + '/public')));
+
+//ROUTES
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 3000;
-app.listen(port, (req) => {
-    console.log(`sunucu ${port} ile çalışıyor`);
+app.listen(port, () => {
+  console.log(`sunucu ${port} ile çalışıyor`);
 });
